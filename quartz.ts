833fe4349e10d447f5c0eed58ce26733634a5ab7
Daniele Trainini <dani84bs@gmail.com>
2026-07-15 17:15:10 +0200

    feat(explorer): scope sidebar tree to notes/ only

--- quartz.ts
+++ quartz.ts
@@ -1,4 +1,15 @@
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
+import * as ExternalPlugin from "./.quartz/plugins"
+
+const EXCLUDED_EXPLORER_FOLDERS = ["posts", "private", "templates"]
+
+ExternalPlugin.Explorer({
+  filterFn: (node) => {
+    if (!node.isFolder) return true
+    const topLevelFolder = node.slugSegments?.[0]
+    return !EXCLUDED_EXPLORER_FOLDERS.includes(topLevelFolder ?? "")
+  },
+})
 
 const config = await loadQuartzConfig()
 export default config
